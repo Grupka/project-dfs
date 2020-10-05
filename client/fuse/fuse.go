c673fb4946b0ec6d90860928e0a8626e8f462194
IamMaxim <g.maxim.stepanoff@gmail.com>
2020-10-05 21:11:01 +0300

    Working on fuse

--- client/fuse/fuse.go
+++ client/fuse/fuse.go
@@ -9,6 +9,21 @@ import (
 
 type DfsNode struct {
 	fs.Inode
+	Path     string
+	Content  []byte
+	Children map[uint64]string
+}
+
+func NewDfsNode(path string, content []byte, children map[uint64]string) DfsNode {
+	return DfsNode{
+		Path:     path,
+		Content:  content,
+		Children: children,
+	}
+}
+
+func (node *DfsNode) String() string {
+	return "DfsNode{" + node.Path + "}"
 }
 
 // Node types must be InodeEmbedders.
@@ -17,11 +32,15 @@ var _ = (fs.InodeEmbedder)((*DfsNode)(nil))
 // Node types should implement some file system operations. Here we are testing it.
 var _ = (fs.NodeLookuper)((*DfsNode)(nil))
 var _ = (fs.NodeOpener)((*DfsNode)(nil))
+var _ = (fs.NodeGetattrer)((*DfsNode)(nil))
+var _ = (fs.NodeCreater)((*DfsNode)(nil))
 
 func Mount(path string, node *DfsNode) *fuse.Server {
 	fmt.Println("Mounting root to", path)
 
-	server, err := fs.Mount(path, node, &fs.Options{})
+	server, err := fs.Mount(path, node, &fs.Options{
+		MountOptions: fuse.MountOptions{Debug: true},
+	})
 	if err != nil {
 		log.Fatal(err)
 	}
--- client/fuse/operations.go
+++ client/fuse/operations.go
@@ -8,51 +8,74 @@ import (
 	"log"
 	"syscall"
 	"time"
+	"unsafe"
 )
 
-var dirs = map[uint64]string{2: "dir1", 3: "dir2"}
-
 type bytesFileHandle struct {
-	content []byte
+	Node *DfsNode
 }
 
-// bytesFileHandle allows reads
-var _ = (fs.FileReader)((*bytesFileHandle)(nil))
+//=== General part ===//
+
+func (node *DfsNode) Getattr(ctx context.Context, f fs.FileHandle, out *fuse.AttrOut) syscall.Errno {
+	out.Size = 9999
+	out.Mode = 0777
+	return 0
+}
 
 //=== Files part ===//
 
-func (*DfsNode) Open(ctx context.Context, flags uint32) (fh fs.FileHandle, fuseFlags uint32, errno syscall.Errno) {
-	fmt.Println("Open:", ctx, "; flags =", flags)
+func (node *DfsNode) Open(ctx context.Context, flags uint32) (fh fs.FileHandle, fuseFlags uint32, errno syscall.Errno) {
+	//fmt.Println("Open:", node, "; flags =", flags)
 
-	now := time.Now().Format(time.StampNano) + "\n"
-	fh = &bytesFileHandle{
-		content: []byte(now),
-	}
+	fh = &bytesFileHandle{Node: node}
 
 	// Return FOPEN_DIRECT_IO so content is not cached.
 	return fh, fuse.FOPEN_DIRECT_IO, 0
 }
 
-func (fh *bytesFileHandle) Read(ctx context.Context, dest []byte, off int64) (fuse.ReadResult, syscall.Errno) {
-	fmt.Println("Read:", fh, "; off =", off, "; ctx =", ctx)
+func (h *bytesFileHandle) Read(ctx context.Context, dest []byte, off int64) (fuse.ReadResult, syscall.Errno) {
+	return h.Node.Read(ctx, dest, off)
+}
+
+func (node *DfsNode) Read(ctx context.Context, dest []byte, off int64) (fuse.ReadResult, syscall.Errno) {
+	//fmt.Println("Read:", fh, "; off =", off, "; ctx =", ctx)
 
 	end := off + int64(len(dest))
-	if end > int64(len(fh.content)) {
-		end = int64(len(fh.content))
+	if end > int64(len(node.Content)) {
+		end = int64(len(node.Content))
 	}
 
 	// We could copy to the `dest` buffer, but since we have a
 	// []byte already, return that.
-	return fuse.ReadResultData(fh.content[off:end]), 0
+	return fuse.ReadResultData(node.Content[off:end]), 0
+}
+
+func (h *bytesFileHandle) Write(ctx context.Context, data []byte, off int64) (written uint32, errno syscall.Errno) {
+	return h.Node.Write(ctx, data, off)
+}
+
+func (node *DfsNode) Write(ctx context.Context, data []byte, off int64) (written uint32, errno syscall.Errno) {
+	//fmt.Println("Write:", fh, "; off =", off, "; ctx =", ctx, "data =", data)
+
+	// Extend the content size if needed
+	end := off + int64(len(data))
+	if end > int64(len(node.Content)) {
+		additionalArray := make([]byte, end-int64(len(node.Content)))
+		node.Content = append(node.Content, additionalArray...)
+	}
+
+	copy(node.Content[off:end], data)
+	return uint32(len(data)), 0
 }
 
 //=== Directories part ===//
 
 func (node *DfsNode) Readdir(ctx context.Context) (fs.DirStream, syscall.Errno) {
-	fmt.Println("Readdir:", node.Inode.String())
+	//fmt.Println("Readdir:", node.Inode.String())
 
-	r := make([]fuse.DirEntry, 0, len(dirs))
-	for ino, name := range dirs {
+	r := make([]fuse.DirEntry, 0, len(node.Children))
+	for ino, name := range node.Children {
 		d := fuse.DirEntry{
 			Name: name,
 			Ino:  ino,
@@ -68,11 +91,9 @@ func (node *DfsNode) Readdir(ctx context.Context) (fs.DirStream, syscall.Errno) 
 }
 
 func (node *DfsNode) Lookup(ctx context.Context, name string, out *fuse.EntryOut) (*fs.Inode, syscall.Errno) {
-	fmt.Println("Lookup: name =", name, "; Parent node:", node, "; EntryOut:", out)
-
 	// Check the list of files for the requested name
 	ino := uint64(0)
-	for _ino, _name := range dirs {
+	for _ino, _name := range node.Children {
 		if _name == name {
 			ino = _ino
 			break
@@ -91,7 +112,7 @@ func (node *DfsNode) Lookup(ctx context.Context, name string, out *fuse.EntryOut
 		// inode, they are deduplicated on this key.
 		Ino: ino,
 	}
-	operations := &DfsNode{}
+	operations := &DfsNode{Path: node.Path + "/" + name}
 
 	// The NewInode call wraps the `operations` object into an Inode.
 	child := node.NewInode(ctx, operations, stable)
@@ -99,6 +120,45 @@ func (node *DfsNode) Lookup(ctx context.Context, name string, out *fuse.EntryOut
 	// In case of concurrent lookup requests, it can happen that operations !=
 	// child.Operations().
 	return child, 0
+}
+
+func (node *DfsNode) Create(ctx context.Context, name string, flags uint32, mode uint32, out *fuse.EntryOut) (n *fs.Inode, fh fs.FileHandle, fuseFlags uint32, errno syscall.Errno) {
+	path := node.Path + "/" + name
+	ino := FilepathHash(path)
+
+	stable := fs.StableAttr{
+		Mode: fuse.S_IFREG,
+		// The child inode is identified by its Inode number.
+		// If multiple concurrent lookups try to find the same
+		// inode, they are deduplicated on this key.
+		Ino: ino,
+	}
+
+	now := time.Now().Format(time.StampNano) + "\n"
+
+	operations := NewDfsNode(path, []byte(now), map[uint64]string{})
+	child := node.NewInode(ctx, &operations, stable)
+
+	node.Children[ino] = name
+
+	return child, fh, 0, 0
+}
+
+func (node *DfsNode) Rename(ctx context.Context, name string, newParent fs.InodeEmbedder, newName string, flags uint32) syscall.Errno {
+	newNodeNode := newParent.EmbeddedInode()
+	// TODO: replace with something more elegant?
+	newNode := *(*DfsNode)(unsafe.Pointer(newNodeNode))
+
+	newHash := FilepathHash(newName)
+	hash := FilepathHash(name)
+
+	//newNode.AddChild(newName, node.GetChild(name), true)
+	node.MvChild(name, newParent.EmbeddedInode(), newName, true)
+
+	newNode.Children[newHash] = node.Children[hash]
+	delete(node.Children, hash)
+
+	return 0
 }
 
 //=== Locks part ===//
