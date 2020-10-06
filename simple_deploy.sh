dce37a91a3a9273c00445faa8471a7a035266b00
IamMaxim <g.maxim.stepanoff@gmail.com>
2020-10-06 12:20:54 +0300

    Some changes

--- simple_deploy.sh
+++ simple_deploy.sh
@@ -13,18 +13,23 @@ export STORAGE_SERVER_1_PORT="1967"
 export STORAGE_SERVER_2_PORT="1968"
 
 bash -c "cd run/naming_server \
-  && ADDRESS=0.0.0.0:${NAMING_SERVER_PORT} ../../NamingServer" &
-bash -c "cd run/naming_server \
   && ADDRESS=0.0.0.0:${NAMING_SERVER_PORT} \
-     NAMING_SERVER_ADDRESS=${NAMING_SERVER_ADDRESS}:${NAMING_SERVER_PORT}
      ../../NamingServer" &
-bash -c "cd run/naming_server \
-  && ADDRESS=0.0.0.0:${NAMING_SERVER_PORT} \
-  NAMING_SERVER_ADDRESS=${NAMING_SERVER_ADDRESS}:${NAMING_SERVER_PORT}
-  ../../NamingServer" &
+bash -c "cd run/storage_server_1 \
+  && ADDRESS=0.0.0.0:${STORAGE_SERVER_1_PORT} \
+     NAMING_SERVER_ADDRESS=${NAMING_SERVER_ADDRESS}:${NAMING_SERVER_PORT}
+     ../../StorageServer" &
+bash -c "cd run/storage_server_2 \
+  && ADDRESS=0.0.0.0:${STORAGE_SERVER_2_PORT} \
+     NAMING_SERVER_ADDRESS=${NAMING_SERVER_ADDRESS}:${NAMING_SERVER_PORT}
+     ../../StorageServer" &
 
 echo "Servers started. Type anything to stop. Don't forget to press Return in the end."
+
+# Read user input to a dummy variable to wait
+# shellcheck disable=SC2034
 read -r var1
+
 pkill NamingServer
 pkill StorageServer
 echo "All servers killed."
