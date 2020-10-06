0b80474fa09b9c76e54e3901eea3bd6fd4445389
IamMaxim <g.maxim.stepanoff@gmail.com>
2020-10-06 21:02:21 +0300

    Added some comments and remove unneeded code

--- client/fuse/operations.go
+++ client/fuse/operations.go
@@ -2,7 +2,6 @@ package fuse
 
 import (
 	"context"
-	"fmt"
 	"github.com/hanwen/go-fuse/fs"
 	"github.com/hanwen/go-fuse/fuse"
 	"syscall"
@@ -19,18 +18,21 @@ var _ = (fs.FileWriter)((*DfsHandle)(nil))
 
 //=== General part ===//
 
+// In our case, the most important things here are report file size and permissions (mode).
 func (node *DfsNode) Getattr(ctx context.Context, f fs.FileHandle, out *fuse.AttrOut) syscall.Errno {
 	out.Size = uint64(len(node.Content))
 	out.Mode = 0777
 	return 0
 }
 
+// Used to delete regular files.
 func (node *DfsNode) Unlink(ctx context.Context, name string) syscall.Errno {
 	delete(node.Children, name)
 
 	return 0
 }
 
+// Used to delete directories.
 func (node *DfsNode) Rmdir(ctx context.Context, name string) syscall.Errno {
 	delete(node.Children, name)
 
@@ -39,22 +41,21 @@ func (node *DfsNode) Rmdir(ctx context.Context, name string) syscall.Errno {
 
 //=== Files part ===//
 
+// Creates a handle for a file.
 func (node *DfsNode) Open(ctx context.Context, flags uint32) (fh fs.FileHandle, fuseFlags uint32, errno syscall.Errno) {
-	//fmt.Println("Open:", node, "; flags =", flags)
-
 	fh = &DfsHandle{Node: node}
 
 	// Return FOPEN_DIRECT_IO so content is not cached.
 	return fh, fuse.FOPEN_DIRECT_IO, 0
 }
 
+// Reads from a file handle. In our case, simply forwards call to the node.
 func (h *DfsHandle) Read(ctx context.Context, dest []byte, off int64) (fuse.ReadResult, syscall.Errno) {
 	return h.Node.Read(ctx, h, dest, off)
 }
 
+// Reads from the node.
 func (node *DfsNode) Read(ctx context.Context, f fs.FileHandle, dest []byte, off int64) (fuse.ReadResult, syscall.Errno) {
-	//fmt.Println("Read:", fh, "; off =", off, "; ctx =", ctx)
-
 	end := off + int64(len(dest))
 	if end > int64(len(node.Content)) {
 		end = int64(len(node.Content))
@@ -65,15 +66,13 @@ func (node *DfsNode) Read(ctx context.Context, f fs.FileHandle, dest []byte, off
 	return fuse.ReadResultData(node.Content[off:end]), 0
 }
 
+// Writes to a handle. In our case, simply forwards call to the node.
 func (h *DfsHandle) Write(ctx context.Context, data []byte, off int64) (written uint32, errno syscall.Errno) {
-	fmt.Println("handle Write called")
-
 	return h.Node.Write(ctx, h, data, off)
 }
 
+// Writes to a node.
 func (node *DfsNode) Write(ctx context.Context, f fs.FileHandle, data []byte, off int64) (written uint32, errno syscall.Errno) {
-	fmt.Println("node Write called")
-
 	// Extend the content size if needed
 	end := off + int64(len(data))
 	if end > int64(len(node.Content)) {
@@ -87,9 +86,8 @@ func (node *DfsNode) Write(ctx context.Context, f fs.FileHandle, data []byte, of
 
 //=== Directories part ===//
 
+// Lists all nodes in a directory.
 func (node *DfsNode) Readdir(ctx context.Context) (fs.DirStream, syscall.Errno) {
-	//fmt.Println("Readdir:", node.Inode.String())
-
 	r := make([]fuse.DirEntry, 0, len(node.Children))
 	for name, childNode := range node.Children {
 		d := fuse.DirEntry{
@@ -102,10 +100,10 @@ func (node *DfsNode) Readdir(ctx context.Context) (fs.DirStream, syscall.Errno) 
 		r = append(r, d)
 	}
 
-	fmt.Println("Readdir: returned", len(r), "entries")
 	return fs.NewListDirStream(r), 0
 }
 
+// Checks if asked file is located in the asked node.
 func (node *DfsNode) Lookup(ctx context.Context, name string, out *fuse.EntryOut) (*fs.Inode, syscall.Errno) {
 	// Check the list of files for the requested name
 	ino := uint64(0)
@@ -123,23 +121,10 @@ func (node *DfsNode) Lookup(ctx context.Context, name string, out *fuse.EntryOut
 		return nil, syscall.ENOENT
 	}
 
-	//stable := fs.StableAttr{
-	//	Mode: child.Mode(),
-	//	// The child inode is identified by its Inode number.
-	//	// If multiple concurrent lookups try to find the same
-	//	// inode, they are deduplicated on this key.
-	//	Ino: ino,
-	//}
-	//operations := &DfsNode{Path: node.Path + "/" + name}
-	//
-	//// The NewInode call wraps the `operations` object into an Inode.
-	//child := node.NewInode(ctx, operations, stable)
-
-	// In case of concurrent lookup requests, it can happen Sthat operations !=
-	// child.Operations().
 	return child.EmbeddedInode(), 0
 }
 
+// Creates a file.
 func (node *DfsNode) Create(ctx context.Context, name string, flags uint32, mode uint32, out *fuse.EntryOut) (n *fs.Inode, fh fs.FileHandle, fuseFlags uint32, errno syscall.Errno) {
 	path := node.Path + "/" + name
 
@@ -157,15 +142,12 @@ func (node *DfsNode) Create(ctx context.Context, name string, flags uint32, mode
 	return child, fh, 0, 0
 }
 
+// Renames a node (both files and directories).
 func (node *DfsNode) Rename(ctx context.Context, name string, newParent fs.InodeEmbedder, newName string, flags uint32) syscall.Errno {
 	newNodeNode := newParent.EmbeddedInode()
 	// TODO: replace with something more elegant?
 	newNode := *(*DfsNode)(unsafe.Pointer(newNodeNode))
 
-	//newHash := FilepathHash(newName)
-	//hash := FilepathHash(name)
-
-	//newNode.AddChild(newName, node.GetChild(name), true)
 	node.MvChild(name, newParent.EmbeddedInode(), newName, true)
 
 	newNode.Children[newName] = node.Children[name]
@@ -174,14 +156,13 @@ func (node *DfsNode) Rename(ctx context.Context, name string, newParent fs.Inode
 	return 0
 }
 
+// Creates a directory.
 func (node *DfsNode) Mkdir(ctx context.Context, name string, mode uint32, out *fuse.EntryOut) (*fs.Inode, syscall.Errno) {
 	path := node.Path + "/" + name
 
 	stable := fs.StableAttr{
 		Mode: fuse.S_IFDIR,
 	}
-
-	//now := time.Now().Format(time.StampNano) + "\n"
 
 	operations := NewDfsNode(path, make([]byte, 0), map[string]*DfsNode{})
 	child := node.NewInode(ctx, operations, stable)
