fb2315131673074aba742a5270f7ac0e00f8e7ce
IamMaxim <g.maxim.stepanoff@gmail.com>
2020-10-07 20:21:54 +0300

    Client compilation errors fixed

--- client/client.go
+++ client/client.go
@@ -3,22 +3,55 @@ package client
 import (
 	"../pb"
 	"context"
-	"fmt"
 	"google.golang.org/grpc"
 	"log"
+	"math/rand"
 	"os"
 )
 
 type Client struct {
-	namingServerClient pb.NamingServerClient
+	namingServerClient pb.StorageDiscoveryClient
+	storageServers     map[string]pb.FileOperationsManagerClient
 }
 
 func (client *Client) GetStorageServerForPath(path string) pb.FileOperationsManagerClient {
-	return nil
+	discoverResponse, err := client.namingServerClient.Discover(context.Background(), &pb.DiscoverRequest{
+		Path: path,
+	})
+	if err != nil {
+		println("Error while getting storage server for path \""+path+"\":", err)
+		return nil
+	}
+	if len(discoverResponse.StorageInfo) == 0 {
+		println("No storage servers for path \"" + path + "\" found")
+		return nil
+	}
+	randomIndex := rand.Intn(len(discoverResponse.StorageInfo))
+	address := discoverResponse.StorageInfo[randomIndex].Address
+	return client.GetStorageServerByAddress(address)
+}
+
+func (client *Client) GetStorageServerByAddress(address string) pb.FileOperationsManagerClient {
+	server, ok := client.storageServers[address]
+	if ok {
+		// We are already connected to the server, just return the current connection
+		return server
+	}
+
+	// Connect to the server and save it
+	conn, err := grpc.Dial(address)
+	if err != nil {
+		println("Error occurred during connecting to storage server at \""+address+"\":", err)
+		return nil
+	}
+	server = pb.NewFileOperationsManagerClient(conn)
+	client.storageServers[address] = server
+
+	return server
 }
 
 func (client *Client) GetRandomStorageServer() pb.FileOperationsManagerClient {
-	return nil
+	return client.GetStorageServerByAddress("")
 }
 
 func CheckError(err error) {
